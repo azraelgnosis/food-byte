93a83e4c5470aa1dea6faa6fadbb5f3090aef5b7
azraelgnosis <azraelgnosis@gmail.com>
2020-01-07 03:32:50 -0500

    Generate database

--- foodbyte/__init__.py
+++ foodbyte/__init__.py
@@ -1,4 +1,5 @@
 import os
+
 from flask import Flask
 
 def create_app(test_config=None):
@@ -6,7 +7,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'foodbyte.sqlite'),
     )
 
     if test_config is None:
