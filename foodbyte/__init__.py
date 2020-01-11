f1384b60dcc6e44f00da814702429f0fb0c0c01f
azraelgnosis <azraelgnosis@gmail.com>
2020-01-11 17:30:26 -0500

    Finish auth.register() action

--- foodbyte/__init__.py
+++ foodbyte/__init__.py
@@ -11,7 +11,7 @@ def create_app(test_config=None):
     )
 
     if test_config is None:
-        # load the instnace config, if it exists, when not testing
+        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
--- foodbyte/auth.py
+++ foodbyte/auth.py
@@ -15,3 +15,25 @@ def register():
         username = request.form['username']
         password = request.form['password']
         db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = f'User {username} is already registered.'
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+        
+        flash(error)
+
+    return render_template('auth/register.html')
